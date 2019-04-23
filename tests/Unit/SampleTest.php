775ce295f4c7b30923d3ded0ae189fdc5ac9f20b
Marlon Nino <ninomarlonvilla@gmail.com>
2019-04-23 11:21:27 +0800

    init
    Signed-off-by: Marlon Nino <ninomarlonvilla@gmail.com>

---
+++ tests/Unit/SampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class SampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function sample()
+    {
+        $this->assertTrue(true);
+    }
+}
